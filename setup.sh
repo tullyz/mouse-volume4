44427aea6e3067035bdcda4b8b48646fdfb8a218
Shinji F. Tanaka <50986699+tullyz@users.noreply.github.com>
2026-02-17 23:14:00 +0900

    Restart mouse-volume service instead of starting
    Updated service management for mouse-volume.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 echo "Installing mouse for Volumio..."
-npm install node-mouse
-npm install socket.io-client@2.3.1
-npm install input-event
-npm install --save-dev webpack-remove-debug
-node /home/volumio/mouse-volume4/index.js &
+cd /home/volumio/mouse-volume4 || exit 1
+npm install --no-audit --no-fund --legacy-peer-deps
+node /home/volumio/mouse-volume4/index.js & 
 touch /data/manifestUI
 
 # copy the service file
 sudo cp mouse-volume.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable mouse-volume.service
-sudo systemctl start mouse-volume.service
+sudo systemctl restart mouse-volume.service
 
